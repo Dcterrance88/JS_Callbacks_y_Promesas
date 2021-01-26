6ef88e4ae94c404b56152b9804f808c327de43de
Dcterrance88 <>
2021-01-26 15:46:10 -0500

    Protip: Mejorar el uso del await

--- src/js/await.js
+++ src/js/await.js
@@ -1,12 +1,9 @@
-import { buscarHeroeAsync } from "./promesas";
+import { buscarHeroeAsync, buscarHeroe } from "./promesas";
 
 const heroesIds = ['capi', 'iron', 'spider'];
 
+// export const obtenerHeroesArr = async() => await Promise.all(heroesIds.map(buscarHeroe));
+
 export const obtenerHeroesArr = async() => {
-    const heroesArr = [];
-    for (const id of heroesIds) {
-        const heroe =await buscarHeroeAsync(id);
-        heroesArr.push(heroe);
-    }
-    return heroesArr;
+    return await Promise.all(heroesIds.map(buscarHeroe));
 }
