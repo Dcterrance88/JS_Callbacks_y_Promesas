6f565788415745e4aa99e3f88600b643d3a8fd80
Dcterrance88 <>
2021-01-26 16:01:59 -0500

    Manejo de errores en el await

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { obtenerHeroesArr } from './js/await';
+import { obtenerHeroesArr, obtenerHeroeAwait } from './js/await';
 
 console.time('await');
 
-obtenerHeroesArr().then( heroes => {
-    console.table(heroes);
+obtenerHeroeAwait('cap2i').then( heroe => {
+    console.log(heroe);
     console.timeEnd('await');
-});
+}).catch(console.warn);
