193bab63f739a8f3ce1b3198a2b7ff3154b5196f
Dcterrance88 <>
2021-01-26 11:17:17 -0500

    Callback hell

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './styles.css';
 import { buscarHeroe } from './js/callbacks';
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
 
-buscarHeroe( heroeId, (err, heroe) => {
-    if(err){
-        console.error(err);
-    } else {
-        console.info(heroe);
-    }
+buscarHeroe( heroeId1, (err, heroe1) => {
+    if(err){ return console.error(err); }
+    buscarHeroe( heroeId2, (err, heroe2) => {
+        if(err){ return console.error(err); }
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la misión`);
+    });
 });
 
 /*
